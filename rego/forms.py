268a3190ec9eca64f9f82b59b0a3f77e4a1d204e
Cameron Hyde <cjh059@usc.edu.au>
2019-08-30 11:10:47 +1000

    Header and index complete

--- rego/forms.py
+++ rego/forms.py
@@ -1,7 +1,7 @@
 from .models import Registration
 from django import forms
 
-class RegoForm(forms.ModelForm)
+class RegoForm(forms.ModelForm):
     class Meta:
         model = Registration
         fields = ['name', 'email']
--- rego/views.py
+++ rego/views.py
@@ -1,21 +1,31 @@
+from datetime import datetime
 from django.shortcuts import render
 from .models import Registration
 from .forms import RegoForm
 
 # Create your views here.
 def index(request):
+
+    if 'registered' in request.session.keys():
+        return render(request, 'rego/registered.html')
+
+    r = len(Registration.objects.all())
+    p = 20 - r
     if request.method == 'POST':
         form = RegoForm(request.POST)
         if form.is_valid():
             # Save stuff
             form.save()
+            request.session['registered'] = True
             return render(request, 'rego/registered.html')
-        return render(request, 'rego/index.html', {'form': form})
-    r = len(Registration.objects.all())
-    if r >= 20 or timeout():
+        return render(request, 'rego/index.html',
+                {'form': form, 'places_left': p})
+    if timeout():
+        return render(request, 'rego/timeout.html')
+    if r >= 20:
         return render(request, 'rego/full.html')
     form = RegoForm()
-    return render(request, 'rego/index.html', {'form': form})
+    return render(request, 'rego/index.html', {'form': form, 'places_left': p})
 
 
 def timeout():
