63c45c4b85a454a3fe52a5c80f35d9434f2dc875
Cameron Hyde <cjh059@usc.edu.au>
2019-08-30 11:52:31 +1000

    Commented out sqlite3 DB

--- register/settings.py
+++ register/settings.py
@@ -64,10 +64,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'register.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
