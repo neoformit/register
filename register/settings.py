2ccadbfcf9652372f567743e90254e85a638ddea
Cameron Hyde <cjh059@usc.edu.au>
2019-08-30 14:47:52 +1000

    Timeout works fine. Final release.

--- register/settings.py
+++ register/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import dj_database_url
 
 NO_PLACES = 20
-TIMEOUT_DATE = '2019-08-29'
+TIMEOUT_DATE = '2019-09-10'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
